f1f2e8ca844bfd9834812071b303dba19062c441
YannickOclock <yannick.kuhn@oclock.io>
2025-01-08 23:46:47 +0100

    [useState] - mise en place d'un formulaire de contact fictif

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,28 +1,54 @@
+import { useState } from "react";
+
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [sentEmail, setSentEmail] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name && email && message) {
+      console.log("ton message est bien parti !", name, email, message);
+      setSentEmail(true);
+    }
+  };
+
   return (
     <>
       <h2>Page de contact</h2>
-      <form>
-        <input
-          type="text"
-          name="name"
-          placeholder="Mettez votre nom"
-          aria-label="Mettez votre nom"
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Mettez votre email"
-          aria-label="Mettez votre email"
-        />
-        <textarea
-          name="bio"
-          placeholder="Ecrivez votre message ici"
-          aria-label="Ecrivez votre message ici"
-          rows={10}
-        />
-        <input type="submit" value="Envoyer le formulaire" />
-      </form>
+      {sentEmail ? (
+        <p>Votre message a bien été envoyé</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Mettez votre nom"
+            aria-label="Mettez votre nom"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Mettez votre email"
+            aria-label="Mettez votre email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+          <textarea
+            name="message"
+            placeholder="Ecrivez votre message ici"
+            aria-label="Ecrivez votre message ici"
+            rows={10}
+            value={message}
+            onChange={(e) => setMessage(e.currentTarget.value)}
+          />
+          <input type="submit" value="Envoyer le formulaire" />
+        </form>
+      )}
     </>
   );
 }
